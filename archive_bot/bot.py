0cc841dc12e8aa570947978b3616c000b98cca80
Dima Kuznetsov <dmitrykuzn@gmail.com>
2021-07-24 16:30:28 +0300

    Add decryption of password protected PDFs

--- archive_bot/bot.py
+++ archive_bot/bot.py
@@ -4,6 +4,7 @@ import os
 import pathlib
 import typing
 
+import pikepdf
 import telegram as tg
 import telegram.ext
 
@@ -13,16 +14,24 @@ class Config:
     token: str
     dest_dir: str
     allowed_users: typing.Iterable[str]
+    passwords: typing.Dict[str, str]
 
     @classmethod
     def from_env(cls):
         allowed_users = []
         if os.getenv('ALLOWED_USERS') is not None:
             allowed_users.extend(os.getenv('ALLOWED_USERS').split(','))
+
+        passwords = {}
+        if os.getenv('PDF_PASSWORDS') is not None:
+            for entry in os.getenv('PDF_PASSWORDS').split(','):
+                name, val = entry.split(':', maxsplit=1)
+                passwords[name] = val
         return cls(
             token=os.getenv('TOKEN'),
             dest_dir=os.getenv('DEST_DIR'),
             allowed_users=allowed_users,
+            passwords=passwords,
         )
 
 
@@ -66,16 +75,46 @@ class Bot:
         update.message.document.get_file().download(temp_path)
 
         final_path = self._final_path(filename)
+
+        if not self._process_pdf(update, temp_path):
+            return
+
         os.rename(temp_path, final_path)
         update.message.reply_text(
             f'{filename} stored successfully.'
         )
 
-    def _temp_path(self, filename):
+    def _temp_path(self, filename: str) -> str:
         return f'{self._config.dest_dir}/._{filename}'
 
-    def _final_path(self, filename):
+    def _final_path(self, filename: str) -> str:
         return f'{self._config.dest_dir}/{filename}'
+
+    def _process_pdf(self, update, path):
+        if not _is_password_protected(path):
+            return True
+
+        for name, password in self._config.passwords.items():
+            try:
+                with pikepdf.open(path, password=password) as f:
+                    f.save(f'{path}.out')
+                    os.rename(f'{path}.out', path)
+                    update.message.reply_text(
+                        f'Decrypted with {name} password',
+                    )
+                    return True
+            except pikepdf._qpdf.PasswordError:
+                continue
+        update.message.reply_text('PDF is encrypted with unknown password')
+        return False
+
+
+def _is_password_protected(path):
+    try:
+        with pikepdf.open(path):
+            return False
+    except pikepdf._qpdf.PasswordError:
+        return True
 
 
 def main() -> None:
